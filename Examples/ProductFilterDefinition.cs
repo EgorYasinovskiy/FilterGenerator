fcecfab4eedeb0bfe687247792046e8ac5ec8e17
Егор Ясиновский <e.a.yasinovskiy@yandex.ru>
2026-07-27 03:42:19 +0300

    example of filter with collection of values to find any from this value

--- Examples/ProductFilterDefinition.cs
+++ Examples/ProductFilterDefinition.cs
@@ -30,11 +30,19 @@ public partial class ProductFilterDefinition
         o => new[] { o.Name, o.Description, o.Category.Name };
 
     public string? Tag { get; set; }
+    public List<Guid>? CategoryIds { get; set; }
 
     public static Expression<Func<Product, bool>>? HasTag(ProductFilterDefinition filter)
     {
         return string.IsNullOrEmpty(filter.Tag)
             ? null
             : o => o.Tags.Any(t => t.Tag == filter.Tag);
+    }
+
+    public static Expression<Func<Product, bool>>? InCategories(ProductFilterDefinition filter)
+    {
+        return filter.CategoryIds is { Count: > 0 }
+            ? o => filter.CategoryIds.Contains(o.CategoryId!.Value)
+            : null;
     }
 }
